042bb7da11b523da83806954025e736c39a802e6
akapoor <anshul.kapoor@cern.ch>
2022-04-21 18:34:21 +0200

    version 1.01

--- echoall.sh
+++ echoall.sh
@@ -5,14 +5,14 @@ dump_all (){
     echo "=============================================="
     echo "---------------------------------"
     echo "Python Version"
-    echo $(python -c "import sys; print(sys.version_info);" || python -c "import sys; print sys.version_info;")
-    echo $(python -c "import sys; print(sys.version);" || python -c "import sys; print sys.version;")
+    echo $(python -c "import sys; print(sys.version_info);" || python -c "import sys; print sys.version_info;") || echo 'Maybe no python on this machine'
+    echo $(python -c "import sys; print(sys.version);" || python -c "import sys; print sys.version;") || echo 'Maybe no python on this machine'
     echo "---------------------------------"
     echo "Fortran Version"
-    gfortran --version
+    gfortran --version || echo 'Maybe no fortran on this machine'
     echo "---------------------------------"
     echo "gcc Version"
-    gcc --version
+    gcc --version || echo 'Maybe no gcc on this machine'
     echo "---------------------------------"
     echo "System info dump"
     uname -a
@@ -21,14 +21,16 @@ dump_all (){
     lshw -short
     echo "---------------------------------"
     echo "scram arch"
-    cmsarch || echo scram arch could not run on this machine
+    cmsarch || echo 'scram arch could not run on this machine'
     echo "---------------------------------"
     echo "Linux CPU Information"
     lscpu
     echo "---------------------------------"
 }
 echo Ignore the super user warning.
+echo May complain about not having some softwares!
+echo Check SystemInfoDump.txt anyway!
 echo "=============================================="
-dump_all > SystemInfoDump.txt 
+dump_all > SystemInfoDump.txt
+echo "======Written SystemInfoDump.txt=============="
 echo "=============================================="
-
